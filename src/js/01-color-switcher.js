8542cde1e9ce08c05783e7dadb76fe7749f425e1
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2022-10-08 22:17:18 +0600

    hw9 task1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+const timer = () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', true);
+};
+
+btnStart.addEventListener('click', timer);
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+});
