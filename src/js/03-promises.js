5634d2b3515adbdc25a42e5f272f3f647b0b14e6
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2022-10-10 10:34:33 +0600

    hw9 fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', async event => {
   const step = parseInt(inputStep.value);
   const amount = parseInt(inputAmount.value);
 
-  for (let i = 0; i < amount; i++) {
+  for (let i = 1; i < amount; i++) {
     /**
      * Другой вариант решения
      */
